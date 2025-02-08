17bd4fa5b646ea961f9ae4f5aef7272892979aca
Kaleb Hedrick <57691161+KalebHedrick@users.noreply.github.com>
2025-02-08 03:56:58 -0500

    u

--- LizaLove/src/images.js
+++ LizaLove/src/images.js
@@ -1,4 +1,4 @@
-const images = ["LizaLove/src/assets/beh.jpg", "tylerliza.jpg", "fsefsfe.jpg", "beh.jpg", "cuteliza2.jpg", "cuteliza3.jpg", "drg.jpg", "ehe.jpg", "esf.jpg", "fsefsef.jpg",
+const images = ["hatphoto.jpg", "tylerliza.jpg", "fsefsfe.jpg", "beh.jpg", "cuteliza2.jpg", "cuteliza3.jpg", "drg.jpg", "ehe.jpg", "esf.jpg", "fsefsef.jpg",
                 "heh.jpg", "hgere.jpg", "hrth.jpg", "htrhrt.jpg", "rfdtjftjtfftt.jpg", "jrjr.jpg", "nbrttrh.jpg", "sergrsg.jpg", "sgfs.jpg", "srg.jpg", "tynn.jpg", "yy;jfif.jfif",
             "s1.jpg","s2.jpg","s3.jpg","s4.jpg","s5.jpg","s6.jpg","s7.jpg","s8.jpg","s9.jpg","s10.jpg","s11.jpg","s12.jpg","s13.jpg","s14.jpg","s15.jpg","s16.jpg","s17.jpg","s18.jpg","s19.jpg",
         "s20.jpg", "heart.jpg"]
